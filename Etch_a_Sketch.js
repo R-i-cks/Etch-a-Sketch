6cf6c47fe0494ea4bceb330ec6c9de5bccd559fa
Ricardo Araujo <ricarlojo@gmail.com>
2024-01-25 22:21:55 +0000

    Extras

--- Etch_a_Sketch.js
+++ Etch_a_Sketch.js
@@ -2,6 +2,7 @@ let grid = document.querySelector(".grid");
 
 let rows = 16;
 let cols = 16;
+let colors = false;
 function matrix() {
     grid.innerHTML = '';
     for (let i=0; i< rows; i++){
@@ -11,7 +12,13 @@ function matrix() {
             let cell = document.createElement('div');
             cell.style.width = ((700 / cols)-2) + 'px';
             cell.style.height = ((700 / rows)-2) + 'px';
-            cell.addEventListener("mouseover",function(){ ChangeColor("black", cell)});
+            cell.dataset.brightness = 1;
+            if (colors){
+                let color = '#' + Math.floor(Math.random()*16777215).toString(16);
+                cell.addEventListener("mouseover",function(){ ChangeColor(color, cell)});
+            }else{
+                cell.addEventListener("mouseover",function(){ ChangeColor("black", cell)});
+            }
             cell.classList.add('cell');
             row.appendChild(cell);
         }
@@ -20,6 +27,10 @@ function matrix() {
 }
 function ChangeColor(color, target){
     target.style.backgroundColor = color;
+    let brightness = target.dataset.brightness - 0.1;
+    if (brightness < 0) brightness = 0; 
+    target.style.filter = `brightness(${brightness})`;
+    target.dataset.brightness = brightness;
 }
 
 
@@ -29,4 +40,10 @@ function updateValue(val) {
     cols=val;
     matrix();
 }
+
+function colorSwitch(bol){
+    colors = bol;
+    matrix();
+}
+
 matrix()
